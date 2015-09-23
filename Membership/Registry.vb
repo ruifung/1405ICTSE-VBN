972a5ee4bfacf21c1dc5512599355e86ace1a4dc
Yip Rui Fung <ruifungyip@gmail.com>
2015-09-23 15:52:19 +0800

    Registry added.

---
+++ Membership/Registry.vb
@@ -0,0 +1,13 @@
+﻿Public Class Registry
+    Private userManagerVar As IUserManager
+    Public Property userManager As IUserManager
+        Get
+            Return userManagerVar
+        End Get
+        Set(value As IUserManager)
+            If userManagerVar Is Nothing Then
+                userManagerVar = value
+            End If
+        End Set
+    End Property
+End Class
---
+++ Membership/UI/UserManagement/AddUser.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AddUser
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.username = New System.Windows.Forms.TextBox()
+        Me.password = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'username
+        '
+        Me.username.Location = New System.Drawing.Point(72, 30)
+        Me.username.Name = "username"
+        Me.username.Size = New System.Drawing.Size(100, 20)
+        Me.username.TabIndex = 0
+        '
+        'password
+        '
+        Me.password.Location = New System.Drawing.Point(72, 57)
+        Me.password.Name = "password"
+        Me.password.Size = New System.Drawing.Size(100, 20)
+        Me.password.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(11, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 60)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Password"
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(97, 88)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 4
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(14, 88)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 5
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(81, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Add a new user"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.Location = New System.Drawing.Point(11, 114)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(161, 24)
+        Me.lblStatus.TabIndex = 7
+        Me.lblStatus.Text = "Label4"
+        Me.lblStatus.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'AddUser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(179, 132)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.password)
+        Me.Controls.Add(Me.username)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "AddUser"
+        Me.Text = "Add New User"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents username As TextBox
+    Friend WithEvents password As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblStatus As Label
+End Class

551b27e0d4129f9c99bc5dfbd1b0240a19e92bbd
Yip Rui Fung <ruifungyip@gmail.com>
2015-09-23 14:41:00 +0800

    Some Interfaces.

---
+++ Membership/DB/API/Users/IUser.vb
@@ -0,0 +1,6 @@
+﻿Public Interface IUser
+    ReadOnly Property userID As Integer
+    Property userName As String
+    WriteOnly Property password As String
+    Function checkPassword(password As String) As Boolean
+End Interface
---
+++ Membership/DB/API/Users/IUserManager.vb
@@ -0,0 +1,5 @@
+﻿Public Interface IUserManager
+    Function getUser(name As String) As IUser
+    Function addUser(name As String, pass As String) As Boolean
+    Function delUser(name As String) As Boolean
+End Interface

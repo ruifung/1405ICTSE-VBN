013bc2926a10d51b57297b34550d055db902349f
Yip Rui Fung <ruifungyip@gmail.com>
2015-10-29 23:50:47 +0800

    Better salting.

--- Membership/Data/DB/Users.vb
+++ Membership/Data/DB/Users.vb
@@ -10,6 +10,7 @@ Partial Public Class DB
         UsersTable.Fields("id") = New Field(MDBType.AutoNumber)
         UsersTable.Fields("username") = New Field(MDBType.Text, 20)
         UsersTable.Fields("authkey") = New Field(MDBType.Binary, 32)
+        UsersTable.Fields("salt") = New Field(MDBType.Binary, 16)
         UsersTable.Fields("permissions") = New Field(MDBType.Number)
         UsersTable.PrimaryKey = "id"
         DB.RegisterTable(UsersTable)
@@ -47,7 +48,10 @@ Partial Public Class DB
                 Return Convert.ToBase64String(CType(Me("authkey"), Byte()))
             End Get
             Set(value As String)
-                Me("authkey") = hash(value, userName)
+                Dim salt(16) As Byte
+                RNGCryptoServiceProvider.Create().GetBytes(salt)
+                Me("salt") = salt
+                Me("authkey") = hash(Encoding.UTF8.GetBytes(value), salt)
             End Set
         End Property
         ReadOnly Property passwordHashed As Boolean _
@@ -65,16 +69,15 @@ Partial Public Class DB
             End Set
         End Property
         Public Function validate(pass As String) As Boolean Implements IUser.verifyPass
-            Return hash(pass, userName).SequenceEqual(CType(Me("authkey"), IEnumerable(Of Byte)))
+            Return hash(Encoding.UTF8.GetBytes(pass),
+                        CType(Me("salt"), Byte())) _
+                            .SequenceEqual(CType(Me("authkey"), IEnumerable(Of Byte)))
         End Function
 
-        Private Function hash(str As String, salt As String) As Byte()
-            Dim saltPos As Integer = CInt(Math.Round(Math.Min(Math.Max(
-                (str.Length + salt.Length) / 2, 0), str.Length)))
-            Dim salted = String.Concat(str.Substring(0, saltPos), salt, str.Substring(saltPos))
-            Dim bytes = Encoding.UTF8.GetBytes(salted.ToCharArray())
+        Private Function hash(toHash As Byte(), salt As Byte()) As Byte()
+            Dim salted As Byte() = CType(toHash.Concat(salt), Byte())
             Dim sha = SHA256Managed.Create()
-            Return sha.ComputeHash(bytes)
+            Return sha.ComputeHash(salted)
         End Function
     End Class
 End Class

9742397ab88b6b7a215f137d43e39f232d58afbe
ruifung <ruifungyip@gmail.com>
2015-11-03 23:49:31 +0800

    UI work

--- Membership/UI/MemberManagement/ModifyMemberDialog.Designer.vb
+++ Membership/UI/MemberManagement/ModifyMemberDialog.Designer.vb
@@ -24,14 +24,34 @@ Partial Class ModifyMemberDialog
     Private Sub InitializeComponent()
         Me.tabView = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.btnCancel = New System.Windows.Forms.Button()
         Me.btnAccept = New System.Windows.Forms.Button()
         Me.memberDetailsView = New Membership.MemberDetails()
         Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.checked = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.ID = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Charges = New System.Windows.Forms.DataGridViewComboBoxColumn()
+        Me.Remarks = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Payable = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Paid = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Balance = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.tabView.SuspendLayout()
         Me.TabPage1.SuspendLayout()
         Me.TabPage2.SuspendLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.FlowLayoutPanel1.SuspendLayout()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -39,28 +59,40 @@ Partial Class ModifyMemberDialog
         '
         Me.tabView.Controls.Add(Me.TabPage1)
         Me.tabView.Controls.Add(Me.TabPage2)
-        Me.tabView.Location = New System.Drawing.Point(2, 3)
+        Me.tabView.Cursor = System.Windows.Forms.Cursors.Default
+        Me.tabView.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tabView.Location = New System.Drawing.Point(0, 0)
         Me.tabView.Name = "tabView"
         Me.tabView.SelectedIndex = 0
-        Me.tabView.Size = New System.Drawing.Size(465, 349)
+        Me.tabView.Size = New System.Drawing.Size(758, 402)
         Me.tabView.TabIndex = 57
         '
         'TabPage1
         '
+        Me.TabPage1.Controls.Add(Me.Button1)
         Me.TabPage1.Controls.Add(Me.btnCancel)
         Me.TabPage1.Controls.Add(Me.btnAccept)
         Me.TabPage1.Controls.Add(Me.memberDetailsView)
         Me.TabPage1.Location = New System.Drawing.Point(4, 22)
         Me.TabPage1.Name = "TabPage1"
         Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(457, 323)
+        Me.TabPage1.Size = New System.Drawing.Size(750, 376)
         Me.TabPage1.TabIndex = 0
         Me.TabPage1.Text = "Member Profile"
         Me.TabPage1.UseVisualStyleBackColor = True
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(202, 286)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 62
+        Me.Button1.Text = "Edit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'btnCancel
         '
-        Me.btnCancel.Location = New System.Drawing.Point(291, 297)
+        Me.btnCancel.Location = New System.Drawing.Point(283, 286)
         Me.btnCancel.Name = "btnCancel"
         Me.btnCancel.Size = New System.Drawing.Size(75, 23)
         Me.btnCancel.TabIndex = 61
@@ -69,7 +101,7 @@ Partial Class ModifyMemberDialog
         '
         'btnAccept
         '
-        Me.btnAccept.Location = New System.Drawing.Point(372, 297)
+        Me.btnAccept.Location = New System.Drawing.Point(364, 286)
         Me.btnAccept.Name = "btnAccept"
         Me.btnAccept.Size = New System.Drawing.Size(75, 23)
         Me.btnAccept.TabIndex = 60
@@ -79,33 +111,158 @@ Partial Class ModifyMemberDialog
         'memberDetailsView
         '
         Me.memberDetailsView.BoundMember = Nothing
-        Me.memberDetailsView.Location = New System.Drawing.Point(12, 12)
+        Me.memberDetailsView.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.memberDetailsView.Location = New System.Drawing.Point(3, 3)
         Me.memberDetailsView.Name = "memberDetailsView"
-        Me.memberDetailsView.Size = New System.Drawing.Size(442, 303)
+        Me.memberDetailsView.Size = New System.Drawing.Size(744, 370)
         Me.memberDetailsView.TabIndex = 0
         '
         'TabPage2
         '
-        Me.TabPage2.Controls.Add(Me.DataGridView1)
+        Me.TabPage2.Controls.Add(Me.SplitContainer1)
         Me.TabPage2.Location = New System.Drawing.Point(4, 22)
         Me.TabPage2.Name = "TabPage2"
         Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(457, 323)
+        Me.TabPage2.Size = New System.Drawing.Size(750, 376)
         Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "Transactions"
+        Me.TabPage2.Text = "Billing"
         Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1
+        Me.SplitContainer1.Location = New System.Drawing.Point(3, 3)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.FlowLayoutPanel1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.TextBox1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.DataGridView1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(744, 370)
+        Me.SplitContainer1.SplitterDistance = 28
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.Controls.Add(Me.Button2)
+        Me.FlowLayoutPanel1.Controls.Add(Me.Button4)
+        Me.FlowLayoutPanel1.Controls.Add(Me.Button3)
+        Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(265, 28)
+        Me.FlowLayoutPanel1.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Button2.Location = New System.Drawing.Point(3, 3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(86, 23)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "Add Payment"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(95, 3)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(85, 23)
+        Me.Button4.TabIndex = 2
+        Me.Button4.Text = "Show Unpaid"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.AccessibleRole = System.Windows.Forms.AccessibleRole.None
+        Me.Button3.Location = New System.Drawing.Point(186, 3)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(76, 23)
+        Me.Button3.TabIndex = 1
+        Me.Button3.Text = "History"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(557, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Member Credit:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(641, 5)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
         '
         'DataGridView1
         '
         Me.DataGridView1.AllowUserToAddRows = False
         Me.DataGridView1.AllowUserToDeleteRows = False
-        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.Control
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.checked, Me.ID, Me.Charges, Me.Remarks, Me.Payable, Me.Paid, Me.Balance})
         Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.DataGridView1.Location = New System.Drawing.Point(3, 3)
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(451, 317)
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(744, 338)
         Me.DataGridView1.TabIndex = 0
+        '
+        'checked
+        '
+        Me.checked.HeaderText = ""
+        Me.checked.Name = "checked"
+        Me.checked.ReadOnly = True
+        '
+        'ID
+        '
+        Me.ID.HeaderText = "ID"
+        Me.ID.Name = "ID"
+        Me.ID.ReadOnly = True
+        '
+        'Charges
+        '
+        Me.Charges.HeaderText = "Charges"
+        Me.Charges.Name = "Charges"
+        Me.Charges.ReadOnly = True
+        '
+        'Remarks
+        '
+        Me.Remarks.HeaderText = "Remarks"
+        Me.Remarks.Name = "Remarks"
+        Me.Remarks.ReadOnly = True
+        '
+        'Payable
+        '
+        Me.Payable.HeaderText = "Total Payable"
+        Me.Payable.Name = "Payable"
+        Me.Payable.ReadOnly = True
+        '
+        'Paid
+        '
+        Me.Paid.HeaderText = "Total Paid"
+        Me.Paid.Name = "Paid"
+        Me.Paid.ReadOnly = True
+        '
+        'Balance
+        '
+        Me.Balance.HeaderText = "Balance"
+        Me.Balance.Name = "Balance"
+        Me.Balance.ReadOnly = True
         '
         'ModifyMemberDialog
         '
@@ -113,13 +270,20 @@ Partial Class ModifyMemberDialog
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoSize = True
         Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.ClientSize = New System.Drawing.Size(468, 352)
+        Me.ClientSize = New System.Drawing.Size(758, 402)
         Me.Controls.Add(Me.tabView)
+        Me.MinimumSize = New System.Drawing.Size(512, 0)
         Me.Name = "ModifyMemberDialog"
         Me.Text = "Member Profile"
         Me.tabView.ResumeLayout(False)
         Me.TabPage1.ResumeLayout(False)
         Me.TabPage2.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.FlowLayoutPanel1.ResumeLayout(False)
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
@@ -131,5 +295,20 @@ Partial Class ModifyMemberDialog
     Friend WithEvents memberDetailsView As MemberDetails
     Friend WithEvents btnCancel As Button
     Friend WithEvents btnAccept As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
     Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents checked As DataGridViewCheckBoxColumn
+    Friend WithEvents ID As DataGridViewTextBoxColumn
+    Friend WithEvents Charges As DataGridViewComboBoxColumn
+    Friend WithEvents Remarks As DataGridViewTextBoxColumn
+    Friend WithEvents Payable As DataGridViewTextBoxColumn
+    Friend WithEvents Paid As DataGridViewTextBoxColumn
+    Friend WithEvents Balance As DataGridViewTextBoxColumn
 End Class

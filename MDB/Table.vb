00356d0fb6957af9d4f2dcbb4b31d6294d0ccf82
0nepeop1e <0nepeop1e@moe9th.com>
2015-11-04 19:56:55 +0800

    fixed constraint

--- MDB/Table.vb
+++ MDB/Table.vb
@@ -1,7 +1,7 @@
 ﻿Imports System.Data.OleDb
 Public Class Table
     Private Shared con As String() = {"UNIQUE({0})", "PRIMARY KEY({0})", "FOREIGN KEY({0}) REFERENCES {1}({2})"}
-    Public Name As String, PrimaryKey As String, Constraints As List(Of Constraint)
+    Public Name As String, PrimaryKey As String, Constraints As List(Of Constraint) = New List(Of Constraint)
     Public Fields As Dictionary(Of String, Field) = New Dictionary(Of String, Field)
     Public Function CreateSQL() As String
         Dim sql As String = String.Format("CREATE TABLE `{0}` (", Name)

c7cf9596df7b4a394f31b871d3a642912cf2eb0e
Yip Rui Fung <ruifungyip@gmail.com>
2015-11-05 11:50:53 +0800

    API Update

--- Membership/API/Members/Payments/IPaymentManager.vb
+++ Membership/API/Members/Payments/IPaymentManager.vb
@@ -2,12 +2,20 @@
     'Probably should cache the result from this.
     Function getUnpaidCharges(member As IMember) As HashSet(Of IMemberCharge)
     Function pay(charges As HashSet(Of IMemberCharge), amount As Double) As IMemberPayment
-    'Maybe?
-    Function removePayment(payment As IMemberPayment) As Boolean
 
+    Function removePayment(payment As IMemberPayment) As Boolean
+    ''' <summary>
+    ''' Must have no payment referencing this charge to remove.
+    ''' </summary>
+    ''' <param name="charge">Charge to remove</param>
+    ''' <returns></returns>
+    Function removeCharge(charge As IMemberCharge) As Boolean
+
+    Function getCharge(chargeID As Integer) As IMemberCharge
     Function listCharges(member As IMember, Optional minDate As Date = Nothing, Optional maxDate As Date = Nothing) As HashSet(Of IMemberCharge)
     Function listCharges(memberID As Integer, Optional minDate As Date = Nothing, Optional maxDate As Date = Nothing) As HashSet(Of IMemberCharge)
 
+    Function getPayment(paymentID As Integer) As IMemberPayment
     Function listPayments(member As IMember, Optional minDate As Date = Nothing, Optional maxDate As Date = Nothing) As HashSet(Of IMemberPayment)
     Function listPayments(memberID As Integer, Optional minDate As Date = Nothing, Optional maxDate As Date = Nothing) As HashSet(Of IMemberPayment)
 End Interface

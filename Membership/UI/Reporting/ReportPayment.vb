771819eb02dd606c71237b747fdd7104dda2fb45
Balram Dhaberia <balram960325@gmail.com>
2015-11-06 15:37:26 +0800

    Report Updates

---
+++ Membership/UI/Reporting/ReportPayment.vb
@@ -0,0 +1,7 @@
+﻿Public Class ReportPayment
+    Property memberFname As String
+    Property ID As String
+    Property amountpaid As Decimal
+    Property totalpayable As Decimal
+    Property balance As Decimal
+End Class
